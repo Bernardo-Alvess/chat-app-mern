f6ffa26739cdc55643034abf1af589bf79bd73b8
Bernardo Alves <bernardoalves330@gmail.com>
2024-04-16 11:37:53 -0300

    finished login and logout functions

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,77 +1,92 @@
-import User from "../models/user.model.js"
-import bcrypt from "bcryptjs"
-import generateTokenAndSetCookie from "../utils/generateToken.js"
+import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+import generateTokenAndSetCookie from '../utils/generateToken.js';
 
 export const signup = async (req, res) => {
-    try{
-        const {fullName, username, password, confirmedPassword, gender} = req.body
+	try {
+		const { fullName, username, password, confirmedPassword, gender } = req.body;
 
-        if(password !== confirmedPassword) return res.status(400).json({error: "Password don't match!"})
+		if (password !== confirmedPassword) return res.status(400).json({ error: "Password don't match!" });
 
-        const user = await User.findOne({username})
+		const user = await User.findOne({ username });
 
-        if(user) return res.status(400).send({error: "Username already exists!"})
-        
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password, salt)
+		if (user) return res.status(400).send({ error: 'Username already exists!' });
 
-        let profilePic = ''
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
 
-        switch(gender){
-            case 'female':
-                profilePic = `https://avatar.iran.liara.run/public/girl?username=${username}}`
-                break;
-            case 'male':
-                profilePic = `https://avatar.iran.liara.run/public/boy?username=${username}}`
-                break;
-            case 'other':
-                profilePic = `https://avatar.iran.liara.run/username?username=${fullName}`
-                break
-        }
+		let profilePic = '';
 
-        const newUser = new User({
-            fullName,
-            username,
-            password: hashedPassword,
-            gender,
-            profilePic,
+		switch (gender) {
+			case 'female':
+				profilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+				break;
+			case 'male':
+				profilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+				break;
+			case 'other':
+				profilePic = `https://avatar.iran.liara.run/username?username=${fullName}`;
+				break;
+		}
 
-        })
+		const newUser = new User({
+			fullName,
+			username,
+			password: hashedPassword,
+			gender,
+			profilePic,
+		});
 
-        if(newUser){
-            generateTokenAndSetCookie(newUser._id, res)
-            await newUser.save()
+		if (newUser) {
+			generateTokenAndSetCookie(newUser._id, res);
+			await newUser.save();
 
-            return res.status(201).json({
-                _id: newUser._id,
-                fullName: newUser.fullName,
-                username: newUser.userName,
-                profilePic: newUser.profilePic
-            })
-        }
+			return res.status(201).json({
+				_id: newUser._id,
+				fullName: newUser.fullName,
+				username: newUser.userName,
+				profilePic: newUser.profilePic,
+			});
+		}
 
-        res.status(400).json({ error: 'Invalid user data'})
-    }catch(error){
-
-        console.log('Error in signup controller', error.message)
-        res.status(500).json({error: "Internal server error!"})
-    }
-}
+		res.status(400).json({ error: 'Invalid user data' });
+	} catch (error) {
+		console.log('Error in signup controller', error.message);
+		res.status(500).json({ error: 'Internal server error!' });
+	}
+};
 
 export const login = async (req, res) => {
-    const { username, password} = req.body
+	try {
+		const { username, password } = req.body;
+		const user = await User.findOne({ username });
 
-    const user = await User.findOne({username})
+		const isPasswordRight = await bcrypt.compare(password, user?.password || '');
 
-    if(!user) return res.status(400).json({ error: 'User not found'})
+		if (!isPasswordRight || !user) return res.status(400).json({ error: 'Invalid username or password' });
 
-    const isPasswordRight = await bcrypt.compare(password, user.password)
+		generateTokenAndSetCookie(user._id, res);
 
-    if(!isPasswordRight) return res.status(400).json({ error: "Password does not match!"})
+		return res.status(201).json({
+				_id: user._id,
+				fullName: user.fullName,
+				username: user.userName,
+				profilePic: user.profilePic,
+			});
 
-    res.status(200).json({ msg: "sucess"})
-}
+	} catch (error) {
+		console.log('Error in login controller', error.message);
+		res.status(500).json({ error: 'Internal server error!' });
+	}
+};
 
 export const logout = (req, res) => {
-    console.log("logoutUser")
-}
+	try{
+		res.cookie("jwt", "", { maxAge: 0 });
+		res.status(200).json({ message: 'Logged out succesfully'})
+
+	} catch (error) {
+		console.log('Error in logout controller', error.message);
+		res.status(500).json({ error: 'Internal server error!' });
+	}
+};
