f0475562dba20c35660530879c4ff6f7cdd1d173
Bernardo Alves <bernardoalves330@gmail.com>
2024-05-06 21:22:33 -0300

    message and conversation routes and controllers added

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express from "express"
 import dotenv from "dotenv"
 
 import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/message.routes.js"
 import devRoutes from "./routes/dev.routes.js"
+import cookieParser from "cookie-parser"
 import connectToMongoDB from "./db/connectToMongoBD.js"
 
 const app = express()
@@ -11,7 +13,12 @@ const PORT = process.env.PORT || 5000
 dotenv.config()
 
 app.use(express.json())
+app.use(cookieParser())
+
+app.use(express.json())
 app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
+
 app.use("/api/dev/", devRoutes)
 
 app.get("/", (req, res) => {
