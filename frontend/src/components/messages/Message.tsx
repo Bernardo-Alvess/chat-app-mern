13a3c8fa77f871beaed3ae3a9a172711ac1eeb79
Bernardo Alves <bernardoalves330@gmail.com>
2024-05-14 21:13:11 -0300

    fixed typo

--- frontend/src/components/messages/Message.tsx
+++ frontend/src/components/messages/Message.tsx
@@ -10,7 +10,7 @@ const  Message = () => {
                 <img alt="avatar"/>
             </div> 
         </div>
-        <div className='chat-bubble text-white bg-blue-500'>}</div>
+        <div className='chat-bubble text-white bg-blue-500'></div>
         <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'></div>
     </div>
   )
