0c57cff3558a0f2560f7798971cb9e7f9f8afc21
Bernardo Alves <bernardoalves330@gmail.com>
2024-05-16 20:11:51 -0300

    builded

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,11 +1,9 @@
 import Conversation from './Conversation'
 import useGetConversations from '../../hooks/useGetConversations'
 import { getRandomEmoji } from '../../utils/getRandomEmoji'
-import useListenConversations from '../../hooks/useListenConversations'
 
 const Conversations = () => {
   const { loading, conversations } = useGetConversations()
-  useListenConversations()
   
   return (
     <div className='py-2 flex flex-col overflow-auto'>
